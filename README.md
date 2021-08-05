8cecbb43d190f0ae4cc0a9543b96961c4cd0fa4d
qinyuanpei <yuanpei@wangdou-hbean.com>
2021-08-05 10:54:50 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
 # Grpc.Gateway
+
+一个基于 ASP.NET Core 终结点路由打造的扩展，可以让你像调用一个 JSON API 一样调用 gRpc。
+
+其原理是，通过反射和表达式树为每一个 gRPC 客户端生成动态路由，并由该扩展完成 JSON -> Protobuf -> JSON 的转换。
+
+# 如何使用
+
+* 编写服务
+
+```
+syntax = "proto3";
+
+option csharp_namespace = "ExampleService";
+
+package greet;
+
+// The greeting service definition.
+service Greeter {
+  // Sends a greeting
+  rpc SayHello (HelloRequest) returns (HelloReply);
+}
+
+// The request message containing the user's name.
+message HelloRequest {
+  string name = 1;
+}
+
+// The response message containing the greetings.
+message HelloReply {
+  string message = 1;
+}
+```
+
+请确保该项目可以同时生成 gRPC 客户端和服务端的代码，因为网关中需要用到 gRPC 客户端。
+
+```xml
+<ItemGroup>
+    <Protobuf Include="Protos\greet.proto" GrpcServices="Both" />
+</ItemGroup>
+```
+更多细节，请参考：ExampleService
+
+* 配置网关
+
+```csharp
+public void ConfigureServices(IServiceCollection services)
+{
+
+    // ...
+    services.Configure<KestrelServerOptions>(x => x.AllowSynchronousIO = true);
+    services.Configure<IISServerOptions>(x => x.AllowSynchronousIO = true);
+    services.AddGrpcClients(typeof(GreeterService).Assembly, opt =>
+    {
+        opt.Address = new Uri("https://localhost:8001");
+    });
+}
+
+public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+{
+    // ...
+    app.UseGrpcGateway
+    (
+        typeof(GreeterService).Assembly
+    );
+}
+```
+
+更多细节，请参考：ExampleGateway
+
+* 调用接口
+
+对于 gRPC 客户端 `Greeter.GreeterClient` 的 `SayHelloAsync()` 方法，其生成的默认路由为：`/Greeter/SayHello`，即移除`Client` 和 `Async` 部分。
+
+此时，我们只需要使用 Postman 或者 crul 以 POST 方式调用接口即可，Enjoy :)
+
+
+
+
---
+++ src/Grpc.Gateway/GrpcExtensions.cs
@@ -0,0 +1,149 @@
+﻿using Google.Protobuf;
+using Grpc.Core;
+using Grpc.Net.ClientFactory;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace Grpc.Gateway
+{
+    public static class GrpcExtensions
+    {
+        public static void UseGrpcGateway(this IApplicationBuilder app, params Assembly[] assemblies)
+        {
+            var loggerFactory = app.ApplicationServices.GetService<ILoggerFactory>();
+            var logger = loggerFactory.CreateLogger("GrpcGateway");
+
+            var clientTypes = AggregateGrpcClientTypes(assemblies);
+
+            foreach (var clientType in clientTypes)
+            {
+                var serviceName = clientType.Name.Replace("Client", "");
+                foreach (var method in clientType.GetMethods().Where(x => x.Name.EndsWith("Async") && x.GetParameters().Length == 4))
+                {
+                    var methodName = method.Name.Replace("Async", "");
+                    var grpcRoute = $"{serviceName}/{methodName}";
+                    logger.LogInformation($"Add gRPC Gateway: {grpcRoute}");
+                    app.UseEndpoints(endpoints => endpoints.MapPost($"{grpcRoute}", async context =>
+                    {
+                        using (var streamReader = new StreamReader(context.Request.Body))
+                        {
+                            var client = app.ApplicationServices.GetService(clientType);
+
+                            var payload = await streamReader.ReadToEndAsync();
+                            var requestType = method.GetParameters()[0].ParameterType;
+                            var messageParser = CreateMessageParser(requestType);
+                            dynamic request = CallParseJson(messageParser, payload);
+
+                            dynamic reply = CallRpcMethod(client, method, request);
+
+                            var response = JsonConvert.SerializeObject(reply.ResponseAsync.Result);
+
+                            context.Response.Headers.Add("X-Grpc-Service", $"{serviceName}Service");
+                            context.Response.Headers.Add("X-Grpc-Method", $"{methodName}Async");
+                            context.Response.Headers.Add("X-Grpc-Client", $"{client.GetType().FullName}");
+
+                            await context.Response.Body.WriteAsync(Encoding.UTF8.GetBytes(response));
+                            context.Response.StatusCode = 200;
+                            context.Response.ContentType = "application/json";
+                        }
+                    }));
+                }
+            }
+        }
+
+        public static void AddGrpcClients(this IServiceCollection services, Assembly assembly, Action<GrpcClientFactoryOptions> configureClient)
+        {
+            var clientTypes = AggregateGrpcClientTypes(assembly);
+            foreach (var clientType in clientTypes)
+            {
+                CallAddGrpcClient(clientType, services, configureClient);
+            }
+        }
+
+        public static IEnumerable<Type> AggregateGrpcClientTypes(params Assembly[] assemblies)
+        {
+            return assemblies
+                .SelectMany(x => x.DefinedTypes)
+                .Where(x => x.BaseType != null && x.BaseType.IsGenericType && x.BaseType.GetGenericTypeDefinition() == typeof(ClientBase<>))
+                .ToList();
+        }
+
+        private static dynamic CreateMessageParser(Type type)
+        {
+            // () => new HelloRequest()
+            var constructor = type.GetConstructor(Type.EmptyTypes);
+            var messageNew = Expression.New(constructor);
+            var messageLambda = Expression.Lambda(messageNew, null);
+
+            // var parser = new MessageParser(() => new HelloRequest()); 
+            var factoryType = typeof(Func<>).MakeGenericType(type);
+            var parserType = typeof(MessageParser<>).MakeGenericType(type);
+            constructor = parserType.GetConstructor(new Type[] { factoryType });
+            var parserNew = Expression.New(constructor, messageLambda);
+            var parserLambda = Expression.Lambda(parserNew, null);
+
+            // return parser
+            var parserFactory = parserLambda.Compile();
+            return parserFactory.DynamicInvoke();
+        }
+
+        private static dynamic CallParseJson(object messageParser, string json)
+        {
+            // () => messageParser.ParseJson(json)
+            var parserInstance = Expression.Constant(messageParser);
+            var parseJsonMethod = Expression.Call(
+                parserInstance,
+                messageParser.GetType().GetMethod("ParseJson"),
+                new List<Expression> { Expression.Constant(json) }
+            );
+
+            var lambdaExp = Expression.Lambda(parseJsonMethod, null);
+            var callInvoker = lambdaExp.Compile();
+            return callInvoker.DynamicInvoke();
+        }
+
+        private static dynamic CallRpcMethod(dynamic rpcClient, MethodInfo methodInfo, dynamic request)
+        {
+            // () => client.SayHelloAsync(request)
+            var clientInstance = Expression.Constant(rpcClient, rpcClient.GetType());
+            var parseJsonMethod = Expression.Call(
+                clientInstance,
+                methodInfo,
+                new List<Expression> {
+                    Expression.Constant(request, request.GetType()),
+                    Expression.Constant(null, typeof(Metadata)),
+                    Expression.Constant(null, typeof(Nullable<System.DateTime>)),
+                    Expression.Constant(default(global::System.Threading.CancellationToken)),
+                }
+            );
+
+            var lambdaExp = Expression.Lambda(parseJsonMethod, null);
+            var callInvoker = lambdaExp.Compile();
+            return callInvoker.DynamicInvoke();
+        }
+
+        private static void CallAddGrpcClient(Type type, IServiceCollection services, Action<GrpcClientFactoryOptions> configureClient)
+        {
+            var methodCall = Expression.Call(
+                null,
+                typeof(GrpcClientServiceExtensions).GetMethod("AddGrpcClient", new Type[] { typeof(IServiceCollection), typeof(Action<GrpcClientFactoryOptions>) }).MakeGenericMethod(type),
+                new List<Expression> {
+                    Expression.Constant(services, services.GetType()),
+                    Expression.Constant(configureClient, configureClient.GetType()),
+                }
+            );
+
+            var lambdaExp = Expression.Lambda(methodCall, null);
+            lambdaExp.Compile().DynamicInvoke();
+        }
+    }
+}
